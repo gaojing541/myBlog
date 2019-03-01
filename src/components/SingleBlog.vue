38152cf76374d841eb1bb0d049dee8d2042153b3
king_jing <574896013@qq.com>
2019-03-01 10:31:35 +0800

    myblogVue

---
+++ src/components/SingleBlog.vue
@@ -0,0 +1,21 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name:"single-blog",
+    data(){
+        return{
+            id:null,
+            blog:{} 
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
